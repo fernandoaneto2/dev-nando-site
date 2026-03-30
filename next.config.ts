23bb3afd8ff03f77e43a921887be3b76615155c0
fernandoaneto2 <fernando.neto02@gmail.com>
2026-03-30 14:20:48 -0300

    ajuste hero

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+  output: "export",
+  // Opcional: Se você usa imagens do Next.js (<Image />),
+  // o export estático não suporta a otimização padrão.
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
